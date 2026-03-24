44c0577957c9d51032350e6334811b8ca128cd56
Kyle van de Langemheen <hogir.vandelangemheen@epfl.ch>
2026-03-24 08:11:53 +0100

    feat: add beartype

--- src/semester_project_subliminal_learning/__init__.py
+++ src/semester_project_subliminal_learning/__init__.py
@@ -1,1 +1,8 @@
+try:
+    from beartype.claw import beartype_this_package
+
+    beartype_this_package()
+except ImportError:
+    pass  # beartype not installed, skip runtime type-checking
+
 __version__ = "0.1.0"
